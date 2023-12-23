66702607881a00a5932f522f3220679a4392cf18
ankit-chakraborty <ankitguitar117@gmail.com>
2023-12-24 01:57:33 +0530

    third commit

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("hello world!!!");
+const accountId=232975
+let accountEmail="ankit@gmail.com"
+var accountPassword="2425"
+accountCity= "asansol"
+
+// accountId=4
+
+accountEmail="hc@hc.gmail"
+accountPassword="402357"
+accountCity="delhi"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity])
