f6a8716c2a8428b41f5f042bac0f9d4986818039
ankit-chakraborty <ankitguitar117@gmail.com>
2023-12-25 00:39:58 +0530

    fourth commit

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("at last commited to the codebase using the github terminal!!")
+"use strict";   //treat all the Js code as newer version 
+
+// note that in place of alert we use console.log to print in the terminals
+console.log("helo there");
+
+// the datatypes present in Javascript are:- number of range 2 to the power 53, bigint, string, boolean, null , undefined, symbol(uniqueness in react, figma), object
+
+let score="33abc"
+console.log(typeof score);
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+let isLogged=1
+let booleanLoggedIn=Boolean(isLogged)
+console.log(typeof booleanLoggedIn);
