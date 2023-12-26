110cd7c84aa6a9ebecec7afbf10dbf08dd54d196
ankit-chakraborty <ankitguitar117@gmail.com>
2023-12-27 01:25:56 +0530

    fifth commit

--- app.js
+++ app.js
@@ -13,3 +13,16 @@ console.log(valueInNumber);
 let isLogged=1
 let booleanLoggedIn=Boolean(isLogged)
 console.log(typeof booleanLoggedIn);
+
+// lecture 7 starts:-
+let val=3
+let negvalue=-val
+console.log(negvalue);
+console.log(2**5);
+let str1="good"
+let str2=" morning"
+console.log(str1+str2)
+// prblems that may arise:-
+console.log("1"+ 3)
+console.log("1"+ 3 + 4)
+console.log(1+ 3 + "5")
