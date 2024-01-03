f4187f58214912d071a12b7744b0cde6a2427846
ankit-chakraborty <ankitguitar117@gmail.com>
2024-01-04 02:13:35 +0530

    modified functions file

--- functions.js
+++ functions.js
@@ -49,3 +49,65 @@ function returnSecondArray(getArray){
     return console.log(getArray[2])
 }
 returnSecondArray(array)
+
+
+function one(){
+    const username="github"
+
+    function two(){
+        const qwbsite="github@gmail.com"
+        console.log(username)
+    }
+    // console.log(qwbsite)  this is invalid as parente cannot inherit from child
+    two()
+}
+one()
+
+
+// ++++++++++++++++++++++interesting area ++++++++++++++++++
+
+console.log(addOne(5))
+function addOne(num){
+    return num+1
+}
+
+// console.log(addTwo(6))  ///this will give an error ....this is an example of MINI HOISTING
+const addTwo=function (num){
+    return num+2
+}
+
+
+// ++++++++++++++++ ARROW functions +++++++++
+
+const user={
+    username:"githubslash@gmail.com",
+    payment: true,
+    loggedIn: true,
+    welcomeMessage: function (){
+        console.log(`${this.username} welcome to our website`)
+    }
+}
+
+user.welcomeMessage()
+
+// function test2(){
+   // console.log(this)   //this wont work inside function....this can be accessed only by objects
+// }
+// test2()
+
+// const chai=() =>{
+//     let username="testGit"
+//     // console.log(this.username)  //here also this keyword cannot be used inside the function as well as if we do console.log(this)...this also wont work and give the output of {}
+// }
+// chai()
+
+
+const test2= (num1, num2) =>{
+    return num1+num2
+}
+
+console.log(test2(4,7))
+
+const add3= (num1, num2) =>({
+    userAdd:"yes added"   //when we write the objectswe have to use () in declaring the arrow function
+})
